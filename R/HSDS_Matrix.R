10cdd760ef742e928fd380dc15b51ad0205e3d20
Vince Carey <stvjc@channing.harvard.edu>
2018-08-18 18:50:10 -0400

    modernizing HSDS_Matrix

--- R/HSDS_Matrix.R
+++ R/HSDS_Matrix.R
@@ -6,7 +6,12 @@
 #' HSDS_Matrix
 #' @export
 HSDS_Matrix = function(url, path) {
-  ds = H5S_source(url, path)
+  so = H5S_source(url, path)
+  dss = fetchDatasets(so) 
+  uu = dss$id
+  if (length(uu)>1) message("multiple UUIDs found, using first")
+  uu = uu[1]
+  ds = H5S_dataset2(so, uu)
   DelayedArray(new("H5S_ArraySeed", filepath="", domain="",
    host="", H5S_dataset=ds))
 }
