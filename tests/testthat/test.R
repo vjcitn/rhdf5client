2cb98c49609cac00419ab1afbf4456951486add1
Samuela Pollack <sampoll@users.noreply.github.com>
2018-10-25 15:57:13 -0400

    Removed h5serv from tests

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,23 +1,4 @@
 library(rhdf5client)
-context("connection")
-
-test_that("H5S_source completes", {
- bigec2 = H5S_source(URL_h5serv())
- expect_true(is(bigec2, "H5S_source"))
-})
-
-context("content wrapper structure") 
-
-test_that("H5S_source processes", {
- bigec2 = H5S_source(URL_h5serv())
- expect_true(all(dim(groups(bigec2))==c(2,2))) 
- expect_true(is(links(bigec2,1), "H5S_linkset"))
- expect_true(is(dataset(bigec2, "tenx_100k"), "H5S_dataset"))
- expect_true(is(bigec2[["tenx_100k"]], "H5S_dataset"))
- expect_true(is(dsmeta(bigec2), "DataFrame"))
- expect_true(all(dim(dsmeta(bigec2))==c(2,3))) 
-})
-
 context("indexing infrastructure")
 
 test_that("sproc/isplit work", {
@@ -37,74 +18,33 @@ test_that("sproc/isplit work", {
 
 })
 
-context("targets generation")
-
-test_that("targets method works", {
- bigec2 = H5S_source(URL_h5serv())
- tt = targets(links(bigec2, 1))
- expect_true(length(tt)>=10)     # increased from 7 because tenx_400k added
- expect_true(length(grep("host", tt))>=8) 
-})
-
-context("retrieving data with binary transfer")
-
-test_that("binary transfer works", {
- bigec2 <- H5S_source(URL_h5serv())
- txdat <- bigec2[["tenx_100k_sorted"]]
-
- transfermode(txdat) <- "JSON"
- J <- txdat[15:20, 1905:1906]
- transfermode(txdat) <- "binary"
- B <- txdat[15:20, 1905:1906]
- expect_true(all(J == B))
-
- N <- matrix(c(40, 35, 13, 118, 25, 26, 1, 0, 1, 2, 1, 1), nrow=6, ncol=2, byrow=FALSE)
- expect_true(all(J == N))
-
- M <- txdat[ c(-(1:79999),-(80500:100000)), 4000:4500 ]
- im <- which(M > 200)
- N <- c(378, 271, 255, 204, 1188, 280, 1458, 884, 947)
- expect_true(all(M[im] == N))
-})
-
-context("HSDSSources")
-
-test_that("Both servers found", {
+context("HSDSSource")
+test_that("Server found", {
   src.hsds <- HSDSSource('http://hsdshdflab.hdfgroup.org')
   doms <- listDomains(src.hsds, '/home/spollack')
   expect_true('/home/spollack/testzero.h5' %in% doms) 
-  src.chan <- HSDSSource('http://h5s.channingremotedata.org:5000', 'h5serv')
-  doms <- listDomains(src.chan)
-  expect_true('neurons100k.h5s.channingremotedata.org' %in% doms) 
-  doms <- listDomains(src.chan, 'public/hdfgroup/org')
-  expect_true('tall.public.h5s.channingremotedata.org' %in% doms) 
-  src.fake <- HSDSSource('http://hsdshdflab.fdhgroup.org')
   # catch exception: non-existent source
+  src.fake <- HSDSSource('http://hsdshdflab.fdhgroup.org')
   expect_warning(listDomains(src.fake, '/home'), "bad http request")
 })
 
-context("Files")
+context("HSDSFile")
 test_that("Files can be opened for reading", {
   src.hsds <- HSDSSource('http://hsdshdflab.hdfgroup.org')
-  src.chan <- HSDSSource('http://h5s.channingremotedata.org:5000', 'h5serv')
   f1 <- HSDSFile(src.hsds, '/home/spollack/testzero.h5')
   dsts <- listDatasets(f1)
   expect_true('/grpB/grpBA/dsetX' %in% dsts)
-  f2 <- HSDSFile(src.chan, 'tenx_100k_sorted.h5s.channingremotedata.org')
-  dsts <- listDatasets(f2)
-  expect_true(dsts == c('/assay001'))
   # catch exception: non-existent or empty file
   expect_warning(HSDSFile(src.hsds, '/home/spollack/testfake.h5'), "no datasets")
 })
 
-context("Datasets")
+context("HSDSDataset")
 test_that("Data can be retrieved from Datasets", {
   src.hsds <- HSDSSource('http://hsdshdflab.hdfgroup.org')
-  src.chan <- HSDSSource('http://h5s.channingremotedata.org:5000', 'h5serv')
   f1 <- HSDSFile(src.hsds, '/home/spollack/testone.h5')
-  f2 <- HSDSFile(src.chan, 'tenx_100k_sorted.h5s.channingremotedata.org')
   d1 <- HSDSDataset(f1, '/group0/dset1d')
-  d2 <- HSDSDataset(f2, '/assay001')
+  f2 <- HSDSFile(src.hsds, '/shared/bioconductor/tenx_full.h5')
+  d2 <- HSDSDataset(f2, '/newassay001')
   R <- c(4046,2087,4654,3193)
 
   A <- apply(getData(d2, c('1:4', '1:27998'), transfermode='JSON'), 1, sum)
@@ -119,10 +59,6 @@ test_that("Data can be retrieved from Datasets", {
 context("DelayedArray subclass HSDSArray")
 test_that("DelayedArray can be instantiated and accessed",  {
   R <- c(4046,2087,4654,3193)
-  da <- HSDSArray('http://h5s.channingremotedata.org:5000', 'h5serv', 
-        'tenx_full.h5s.channingremotedata.org', '/newassay001')
-  A <- apply(da[,1:4],2,sum)
-  expect_true(all(R == A))
   da <- HSDSArray('http://hsdshdflab.hdfgroup.org', 'hsds', 
         '/shared/bioconductor/tenx_full.h5', '/newassay001')
   A <- apply(da[,1:4],2,sum)
